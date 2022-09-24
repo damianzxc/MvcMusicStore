d39e9bc97ab1c5279435dbe3636348b0c91c8239
Damian Kaluzny <kaluznydamian@gmail.com>
2022-09-24 16:57:39 +0100

    dodane przykładowe kontrolery

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // Get /Store/
+        public String Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // Get /Store/Browse
+        public String Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        // Get /Store/Details
+        public String Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}

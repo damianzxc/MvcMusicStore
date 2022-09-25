f10dcabba5f55c7ab183f38f324889817eea2036
Damian Kaluzny <kaluznydamian@gmail.com>
2022-09-25 17:04:02 +0100

    Dodano obsługę koszyka

---
+++ MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private MusicStoreEntities storeDb = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up View Model
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var album = storeDb.Albums.Single(a => a.AlbumId == id);
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            
+            cart.AddToCart(album);
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            // Get the name of the album to display confirmation
+            String albumName = storeDb.Carts.Single(item => item.RecordId == id).Album.Title;
+
+            // Remove items and return how many was removed
+            int itemCount = cart.RemoveFromCart(id);
+
+            var result = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) + "has beed removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            }; 
+            return Json(result);
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
